c44f65801d6841e63229f51b50dacc1163314009
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-20 15:23:27 +0100

    Add Field textarea component

---
+++ src/components/shared/field-textarea/FieldTextarea.tsx
@@ -0,0 +1,48 @@
+import styles from './FieldTextarea.module.css';
+import { classNames } from '@/helpers';
+import { Fieldset } from '../fieldset';
+import { FieldTextareaProps } from './types';
+import { Label } from '../label';
+import { Textarea } from '../textarea';
+import { forwardRef, Ref } from 'react';
+
+export const FieldTextarea = forwardRef<
+  HTMLTextAreaElement,
+  FieldTextareaProps
+>(
+  (
+    {
+      children,
+      classNameField,
+      classNameTextarea,
+      classNameLabel,
+      label,
+      name,
+      placeholder,
+      ...rest
+    },
+    ref: Ref<HTMLTextAreaElement>
+  ) => {
+    return (
+      <Fieldset className={classNames(styles.fieldset, classNameField!)}>
+        {label && (
+          <Label
+            className={classNames(styles.label, classNameLabel!)}
+            htmlFor={name}
+          >
+            {label}
+          </Label>
+        )}
+        {children ?? (
+          <Textarea
+            {...rest}
+            className={classNames(styles.input, classNameTextarea!)}
+            name={name}
+            placeholder={placeholder}
+            ref={ref}
+          />
+        )}
+      </Fieldset>
+    );
+  }
+);
