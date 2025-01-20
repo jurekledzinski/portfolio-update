3a4ea5846c18d279bd03e3df8978aa60f790c3e0
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-20 23:50:47 +0100

    Add react hot toast config

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import App from './App.tsx';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
+import { Toaster } from 'react-hot-toast';
 import './index.css';
 
 const queryClient = new QueryClient();
@@ -9,6 +10,15 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Toaster
+        position="top-left"
+        toastOptions={{
+          duration: 2000,
+          style: {
+            borderRadius: 2,
+          },
+        }}
+      />
       <App />
     </QueryClientProvider>
   </StrictMode>
