722d4f3483cd50c147cfde26367bc6396f5f503a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-21 21:50:54 +0100

    Add useMutatiion to post data

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
+import { Slider } from './components/shared';
 import { styles } from '@/styles';
+import { useMutation } from '@tanstack/react-query';
+import { ResponseSuccess, sendData } from './actions';
 import {
   Footer,
   Header,
   IntroductionButtonGroup,
   IntroductionHeader,
 } from '@components/page';
-import { Slider } from './components/shared';
 
 const App = () => {
+  const mutation = useMutation<ResponseSuccess, Error, { eventType: string }>({
+    mutationFn: async (body) => await sendData(body),
+  });
+
   return (
     <>
       <Header />
       <main className={styles.main}>
         <div className={styles.box}>
           <IntroductionHeader />
-          <IntroductionButtonGroup />
+          <IntroductionButtonGroup
+            onDownload={() => {
+              console.log('Click download cv');
+              mutation.mutate({ eventType: 'cv' });
+            }}
+            onRedirectGithub={() => {
+              console.log('Click redirect github');
+              mutation.mutate({ eventType: 'github' });
+            }}
+            onRedirectLinkedin={() => {
+              console.log('Click redirect linkedin');
+              mutation.mutate({ eventType: 'linkedin' });
+            }}
+          />
         </div>
         <div className={styles.box}>
           <header className={styles.header}>
