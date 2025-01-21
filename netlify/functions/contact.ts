35ead13981914d21499ee741807139516856628c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-21 21:33:19 +0100

    Create function for contact

--- netlify/functions/contact.ts
+++ netlify/functions/contact.ts
@@ -1,10 +1,58 @@
-import type { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
+import contactEmailTemplate from '../../src/templates/contactEmail.hbs';
+import fs from 'fs';
+import juice from 'juice';
+import nodemailer from 'nodemailer';
+import path from 'path';
+import { ContactEmailSchema } from '../../src/actions';
+import { tryCatch } from '../../netlify-helpers/tryCatch';
+import type { Handler, HandlerEvent } from '@netlify/functions';
 
-export const handler: Handler = async (
-  event: HandlerEvent,
-  context: HandlerContext
-) => {
-  console.log('Contact', { event }, { context });
+export const handler: Handler = tryCatch(async (event: HandlerEvent) => {
+  const body = JSON.parse(event.body!);
+  const parsedData = ContactEmailSchema.parse(body);
+
+  const currentYear = new Date().getFullYear();
+  const date = new Date().toLocaleString();
+  const htmlContent = contactEmailTemplate({
+    name: parsedData.name,
+    email: parsedData.email,
+    message: parsedData.message,
+    currentYear,
+    date,
+  });
+
+  const cssPath = path.join(
+    process.cwd(),
+    'src',
+    'templates',
+    'contactEmail.css'
+  );
+
+  const cssContent = fs.readFileSync(cssPath, 'utf-8');
+
+  const htmlWithCss = juice.inlineContent(htmlContent, cssContent);
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.HOST_EMAIL,
+    port: process.env.PORT_PROVIDER_EMAIL,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.PASSWORD_USER,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  } as nodemailer.TransportOptions);
+
+  const mailOptions = {
+    from: `Portfolio <${process.env.EMAIL_USER}>`,
+    to: process.env.EMAIL_SENDTO,
+    subject: 'Contact email',
+    html: htmlWithCss,
+  };
+
+  await transporter.sendMail(mailOptions);
 
   return {
     statusCode: 200,
@@ -12,4 +60,4 @@ export const handler: Handler = async (
       message: 'Message sent successfully',
     }),
   };
-};
+});
