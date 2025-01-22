41f438983f15a71612708d5e0124fa02f3b14981
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-22 09:02:32 +0100

    Add buttons live and github

--- src/components/shared/slider/Slider.tsx
+++ src/components/shared/slider/Slider.tsx
@@ -1,7 +1,9 @@
 import styles from './Slider.module.css';
 import useEmblaCarousel from 'embla-carousel-react';
 import { Button, stylesButton } from '@/components/shared';
+import { stylesLink } from '@/components/page';
 import { classNames } from '@/helpers';
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons';
 import { Icon } from '../icon';
 import { sliderData } from '@/data';
 import { SliderProps } from './types';
@@ -13,11 +15,10 @@ import {
 
 export const Slider = ({ options }: SliderProps) => {
   const [emblaRef, emblaApi] = useEmblaCarousel(options);
+  const currentIndex = (emblaApi && emblaApi.selectedScrollSnap()) ?? 0;
 
   const { disabledNext, disabledPrev, onNextButtonClick, onPrevButtonClick } =
     useControlSlider({ emblaApi });
-
-  console.log('emblaApi', emblaApi?.selectedScrollSnap());
 
   return (
     <div className={styles.embla}>
@@ -31,6 +32,34 @@ export const Slider = ({ options }: SliderProps) => {
         </div>
       </div>
       <div className={styles.buttonGroup}>
+        <>
+          <Button
+            className={classNames(
+              stylesButton.buttonConfirm,
+              styles.sliderButton
+            )}
+          >
+            <a
+              className={stylesLink.link}
+              href={sliderData[currentIndex].repository}
+            >
+              Live
+            </a>
+          </Button>
+          <Button
+            className={classNames(
+              stylesButton.buttonConfirm,
+              styles.sliderButton
+            )}
+          >
+            <a
+              className={stylesLink.link}
+              href={sliderData[currentIndex].repository}
+            >
+              <Icon icon={faSquareGithub} />
+            </a>
+          </Button>
+        </>
         <>
           <Button
             className={classNames(
