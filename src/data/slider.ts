9a8a47bc0c4bfd4c41f8393fcfd70937355fe4d0
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-22 08:34:44 +0100

    Create data folder for static data

---
+++ src/data/slider.ts
@@ -0,0 +1,14 @@
+export const sliderData = [
+  {
+    id: 1,
+    src: 'images/shoppy-600.png',
+    live: '',
+    repository: '',
+  },
+  {
+    id: 2,
+    src: 'images/news-600.png',
+    live: '',
+    repository: '',
+  },
+];
