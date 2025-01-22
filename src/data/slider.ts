e1d90596ac60bdcdfb8982bb5d54a39e3839d940
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-01-22 09:03:08 +0100

    Add more data slider

--- src/data/slider.ts
+++ src/data/slider.ts
@@ -2,13 +2,13 @@ export const sliderData = [
   {
     id: 1,
     src: 'images/shoppy-600.png',
-    live: '',
-    repository: '',
+    live: 'https://shoppy-woad-five.vercel.app',
+    repository: 'https://github.com/jurekledzinski/shoppy',
   },
   {
     id: 2,
     src: 'images/news-600.png',
-    live: '',
-    repository: '',
+    live: 'https://daily24news.netlify.app',
+    repository: 'https://github.com/jurekledzinski/Daily-news',
   },
 ];
