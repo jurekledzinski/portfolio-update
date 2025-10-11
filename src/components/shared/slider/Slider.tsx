3fbb6de14bdef170b55fa6841341969526c1d043
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-11 15:02:15 +0200

    Add Image component to Slider

--- src/components/shared/slider/Slider.tsx
+++ src/components/shared/slider/Slider.tsx
@@ -1,5 +1,8 @@
 import styles from './Slider.module.css';
+import { Alert } from '../alert';
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 import { forwardRef } from 'react';
+import { Image, ImageContainer } from '../image';
 import { projectsDescription } from '@/data';
 import { SliderData, SliderProps } from './types';
 
@@ -30,11 +33,36 @@ export const Slider = forwardRef<HTMLDivElement, SliderProps<SliderData>>(
         emblaRef={ref}
         onRenderSlide={(slide, index) => (
           <div className={styles.emblaSlide} key={slide.id}>
+            <ImageContainer key={slide.src} loader="skeleton">
+              {({ onLoad, onError, isError, isLoading }) => (
+                <>
+                  {isError && !isLoading ? (
+                    <Alert
+                      color="negative"
+                      icon={faTriangleExclamation}
+                      message="Failed to load image"
+                      fullWidth
+                      size="size-xs"
+                      variant="light"
+                    />
+                  ) : (
+                    <Image
+                      className={styles.image}
+                      src={slide.src}
+                      onLoad={onLoad}
+                      onError={onError}
+                      alt="Project image"
+                      width={600}
+                      height={597}
+                    />
+                  )}
+                </>
+              )}
+            </ImageContainer>
             <SlideDescription
               data={projectsDescription[currentIndex]}
               isActive={index === selectedIndex}
             />
-            <img src={slide.src} alt="Project image" />
           </div>
         )}
       >
