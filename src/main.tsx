6a19498470f11c19250559dcf0eb50b93ae95a2a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-11 12:05:35 +0200

    Refactor global styles and create folder styles for them

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { Toaster } from 'react-hot-toast';
-import './index.css';
+import './styles/main.css';
 
 const queryClient = new QueryClient();
 
--- src/styles/index.ts
+++
@@ -1,5 +0,0 @@
-import stylesApp from './App.module.css';
-
-export const styles = {
-  ...stylesApp,
-};
