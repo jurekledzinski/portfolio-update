6e974fffd667dec3b291abbda2d38f5cf4b9836f
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-11 13:26:52 +0200

    Update static data for sliders

--- src/data/slider.ts
+++ src/data/slider.ts
@@ -1,19 +1,62 @@
-export const sliderData = [
+export const sliderProjectsData = [
   {
     id: 1,
-    src: 'images/shoppy-600.png',
+    src: 'images/project-houses-600.png',
+    live: 'https://property-marketplace-olive.vercel.app',
+    repository: 'https://github.com/jurekledzinski/property-marketplace',
+  },
+  {
+    id: 2,
+    src: 'images/project-shoppy-600.png',
     live: 'https://shoppy-woad-five.vercel.app',
     repository: 'https://github.com/jurekledzinski/shoppy',
   },
   {
-    id: 2,
-    src: 'images/news-600.png',
+    id: 3,
+    src: 'images/project-news-600.png',
     live: 'https://daily24news.netlify.app',
     repository: 'https://github.com/jurekledzinski/Daily-news',
   },
 ];
 
 export const projectsDescription = [
+  {
+    title: 'Place quest',
+    description:
+      'A modern, responsive marketplace application built with Next.js and TypeScript, featuring secure authentication, advert management, real-time image uploads, and dynamic filtering',
+    techStack: [
+      'Next.js',
+      'Next auth',
+      'React context',
+      'React hook form',
+      'Typescript',
+      'Mongodb',
+      'Jose',
+      'Chart.js React-chartjs-2',
+      'React tanstack query',
+      'React tanstack table',
+      'Imagekit',
+      'Zod',
+    ],
+    features: [
+      'User Authentication & Authorization — Secure login, registration, and session management with NextAuth',
+      'Advert Management — Create, edit, and delete adverts with full validation.',
+      'User Adverts Table — Manage adverts with edit, delete, and activate controls.',
+      'Image Upload Queue — Upload multiple images per advert with: real-time previews, progress indicators, delete functionality',
+      'Server-side Pagination — Efficiently load adverts with backend pagination.',
+      'Advert Filtering, Sorting & Search — Filter adverts by queries, sort by date or price, and search by keywords.',
+      'User Dashboard — Centralized overview of personal stats and active adverts.',
+      'Analytics & Charts — Interactive charts showing advert views and status.',
+      'Profile Management — Securely update user details and change password.',
+      'Account Deletion — Permanently remove user account and related data.',
+      'Messaging System — Send messages from viewers to advertisers.',
+      'Messages Overview & Deletion — View, delete received messages.',
+      'Dynamic Location Selects — Fetch countries, states, and cities dynamically for form selects.',
+      'Image Carousel — Interactive carousel for browsing advert images.',
+      'Dark/Light Theme — User-selectable theme for improved accessibility.',
+      'Responsive Design — Fully optimized layout for mobile, tablet, and desktop devices.',
+    ],
+  },
   {
     title: 'Shoppy',
     description: 'A small e-commerce UI built with Next.js and CSS modules.',
@@ -67,4 +110,281 @@ export const projectsDescription = [
       'Update user profile and change password',
     ],
   },
+];
+
+export const sliderScreenShotsData = [
+  [
+    {
+      id: 1,
+      src: 'images/houses/houses_home_light_1.png',
+    },
+    {
+      id: 2,
+      src: 'images/houses/houses_home_dark_1.png',
+    },
+    {
+      id: 3,
+      src: 'images/houses/houses_details_light_1.png',
+    },
+    {
+      id: 4,
+      src: 'images/houses/houses_details_dark_1.png',
+    },
+    {
+      id: 5,
+      src: 'images/houses/houses_login_light_1.png',
+    },
+    {
+      id: 6,
+      src: 'images/houses/houses_login_dark_1.png',
+    },
+    {
+      id: 7,
+      src: 'images/houses/houses_register_light_1.png',
+    },
+    {
+      id: 8,
+      src: 'images/houses/houses_register_dark_1.png',
+    },
+    {
+      id: 9,
+      src: 'images/houses/houses_dashboard_light_1.png',
+    },
+    {
+      id: 10,
+      src: 'images/houses/houses_dashboard_light_2.png',
+    },
+    {
+      id: 11,
+      src: 'images/houses/houses_dashboard_dark_1.png',
+    },
+    {
+      id: 12,
+      src: 'images/houses/houses_dashboard_dark_2.png',
+    },
+    {
+      id: 13,
+      src: 'images/houses/houses_add_advert_light_1.png',
+    },
+    {
+      id: 14,
+      src: 'images/houses/houses_add_advert_dark_1.png',
+    },
+    {
+      id: 15,
+      src: 'images/houses/houses_edit_advert_light_1.png',
+    },
+    {
+      id: 16,
+      src: 'images/houses/houses_edit_advert_dark_1.png',
+    },
+    {
+      id: 17,
+      src: 'images/houses/houses_table_light_1.png',
+    },
+    {
+      id: 18,
+      src: 'images/houses/houses_table_light_2.png',
+    },
+    {
+      id: 19,
+      src: 'images/houses/houses_table_dark_1.png',
+    },
+    {
+      id: 20,
+      src: 'images/houses/houses_table_dark_2.png',
+    },
+    {
+      id: 21,
+      src: 'images/houses/houses_profile_light_1.png',
+    },
+    {
+      id: 22,
+      src: 'images/houses/houses_profile_dark_1.png',
+    },
+    {
+      id: 23,
+      src: 'images/houses/houses_messages_light_1.png',
+    },
+    {
+      id: 24,
+      src: 'images/houses/houses_messages_light_2.png',
+    },
+    {
+      id: 25,
+      src: 'images/houses/houses_messages_light_3.png',
+    },
+    {
+      id: 26,
+      src: 'images/houses/houses_messages_dark_1.png',
+    },
+    {
+      id: 27,
+      src: 'images/houses/houses_messages_dark_2.png',
+    },
+    {
+      id: 28,
+      src: 'images/houses/houses_messages_dark_3.png',
+    },
+  ],
+  [
+    {
+      id: 1,
+      src: 'images/shoppy/shoppy_home_light_1.png',
+    },
+    {
+      id: 2,
+      src: 'images/shoppy/shoppy_home_dark_1.png',
+    },
+    {
+      id: 3,
+      src: 'images/shoppy/shoppy_list_light_1.png',
+    },
+    {
+      id: 4,
+      src: 'images/shoppy/shoppy_list_dark_1.png',
+    },
+    {
+      id: 5,
+      src: 'images/shoppy/shoppy_details_light_1.png',
+    },
+    {
+      id: 6,
+      src: 'images/shoppy/shoppy_details_dark_1.png',
+    },
+    {
+      id: 7,
+      src: 'images/shoppy/shoppy_sign_in_light_1.png',
+    },
+    {
+      id: 8,
+      src: 'images/shoppy/shoppy_sign_in_dark_1.png',
+    },
+    {
+      id: 9,
+      src: 'images/shoppy/shoppy_sign_up_light_1.png',
+    },
+    {
+      id: 10,
+      src: 'images/shoppy/shoppy_sign_up_dark_1.png',
+    },
+    {
+      id: 11,
+      src: 'images/shoppy/shoppy_contact_light_1.png',
+    },
+    {
+      id: 12,
+      src: 'images/shoppy/shoppy_contact_dark_1.png',
+    },
+    {
+      id: 13,
+      src: 'images/shoppy/shoppy_menu_light_1.png',
+    },
+    {
+      id: 14,
+      src: 'images/shoppy/shoppy_menu_light_2.png',
+    },
+    {
+      id: 15,
+      src: 'images/shoppy/shoppy_menu_dark_1.png',
+    },
+    {
+      id: 16,
+      src: 'images/shoppy/shoppy_menu_dark_2.png',
+    },
+    {
+      id: 17,
+      src: 'images/shoppy/shoppy_recover_light_1.png',
+    },
+    {
+      id: 18,
+      src: 'images/shoppy/shoppy_recover_dark_1.png',
+    },
+    {
+      id: 19,
+      src: 'images/shoppy/shoppy_empty_card_light_1.png',
+    },
+    {
+      id: 20,
+      src: 'images/shoppy/shoppy_empty_card_dark_1.png',
+    },
+    {
+      id: 21,
+      src: 'images/shoppy/shoppy_full_card_light_1.png',
+    },
+    {
+      id: 22,
+      src: 'images/shoppy/shoppy_full_card_dark_1.png',
+    },
+    {
+      id: 23,
+      src: 'images/shoppy/shoppy_list_light_1.png',
+    },
+    {
+      id: 24,
+      src: 'images/shoppy/shoppy_list_dark_1.png',
+    },
+    {
+      id: 25,
+      src: 'images/shoppy/shoppy_orders_light_1.png',
+    },
+    {
+      id: 26,
+      src: 'images/shoppy/shoppy_orders_dark_1.png',
+    },
+    {
+      id: 27,
+      src: 'images/shoppy/shoppy_profile_light_1.png',
+    },
+    {
+      id: 28,
+      src: 'images/shoppy/shoppy_profile_dark_1.png',
+    },
+    {
+      id: 29,
+      src: 'images/shoppy/shoppy_shipping_light_1.png',
+    },
+    {
+      id: 30,
+      src: 'images/shoppy/shoppy_shipping_dark_1.png',
+    },
+    {
+      id: 31,
+      src: 'images/shoppy/shoppy_place_order_light_1.png',
+    },
+    {
+      id: 32,
+      src: 'images/shoppy/shoppy_place_order_dark_1.png',
+    },
+    {
+      id: 33,
+      src: 'images/shoppy/shoppy_payment_stripe.png',
+    },
+  ],
+  [
+    {
+      id: 1,
+      src: 'images/news/news_empty.png',
+    },
+    {
+      id: 2,
+      src: 'images/news/news_home.png',
+    },
+    {
+      id: 3,
+      src: 'images/news/news_list.png',
+    },
+    {
+      id: 4,
+      src: 'images/news/news_details.png',
+    },
+    {
+      id: 5,
+      src: 'images/news/news_sign_in.png',
+    },
+    {
+      id: 6,
+      src: 'images/news/news_sign_up.png',
+    },
+  ],
 ];
