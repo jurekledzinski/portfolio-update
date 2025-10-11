7a9ec22e5c1e4f62c098678a25793ed6c87eab63
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-11 13:24:16 +0200

    Add input component

--- src/components/shared/input/Input.tsx
+++ src/components/shared/input/Input.tsx
@@ -1,15 +1,37 @@
 import styles from './Input.module.css';
 import { classNames } from '@/helpers';
-import { InputProps } from './types';
-import { forwardRef, Ref } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { MergeProps, UnionElements } from './types';
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, ...props }, ref: Ref<HTMLInputElement>) => (
-    <input
-      {...props}
-      className={classNames(styles.input, className!)}
-      id={props.name}
-      ref={ref}
-    />
-  )
+export const Input = forwardRef<UnionElements, MergeProps>(
+  ({ as = 'input', className, ...props }, ref) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const txtRef = useRef<HTMLTextAreaElement>(null);
+
+    useImperativeHandle(ref, () => inputRef.current || txtRef.current!, []);
+
+    if (as === 'textarea' && 'cols' in props) {
+      return (
+        <textarea
+          className={classNames(styles.textarea, className!)}
+          id={props.name}
+          ref={txtRef}
+          {...props}
+        />
+      );
+    }
+
+    if (as === 'input' && 'type' in props) {
+      return (
+        <input
+          className={classNames(styles.input, className!)}
+          id={props.name}
+          ref={inputRef}
+          {...props}
+        />
+      );
+    }
+
+    return null;
+  }
 );
--- src/components/shared/input/types.ts
+++ src/components/shared/input/types.ts
@@ -1,4 +1,15 @@
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  className?: string;
-}
+import { InputHTMLAttributes, TextareaHTMLAttributes } from 'react';
+
+export type InputProps = {
+  as?: 'input';
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export type TextareaProps = {
+  as?: 'textarea';
+  cols?: number;
+  rows?: number;
+} & TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+export type UnionElements = HTMLTextAreaElement | HTMLInputElement;
+
+export type MergeProps = InputProps | TextareaProps;
