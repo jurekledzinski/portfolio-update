4e85fc5ad06a441a6d0dfb8f2999bad8afc0b9e3
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-11 15:00:12 +0200

    Refactor HomePage component

--- src/components/page/home/HomePage.tsx
+++ src/components/page/home/HomePage.tsx
@@ -1,28 +1,14 @@
 import styles from './HomePage.module.css';
-import { Box, Container } from '@/components';
-import {
-  ContainerSliders,
-  Footer,
-  Header,
-  Introduction,
-  SocialLinks,
-} from './components';
+import { Container } from '@/components';
+import { Footer, Header, HeroSection, ProjectsSection } from './components';
 
 export const HomePage = () => {
   return (
     <>
       <Header />
       <Container as="main" className={styles.main} maxWidth="mw-md" p="p-md">
-        <Box className={styles.box}>
-          <Introduction />
-          <SocialLinks />
-        </Box>
-        <Box className={styles.box}>
-          <header className={styles.header}>
-            <h3 className={styles.subTitle}>Projects</h3>
-          </header>
-          <ContainerSliders />
-        </Box>
+        <HeroSection />
+        <ProjectsSection />
       </Container>
       <Footer />
     </>
